966f61894158f5314d1511711293b9879b208df8
Xinx1201 <z975203723@gmai.com>
2022-12-12 00:40:27 +0800

    feat: :construction: TODO autoDelete

--- src/views/Time/index.vue
+++ src/views/Time/index.vue
@@ -19,7 +19,7 @@
         </el-select>
         <el-select v-model="map" filterable placeholder="地图">
           <el-option-group
-            v-for="group in mapOptinos"
+            v-for="group in horseMapOptions"
             :key="group.label"
             :label="group.label"
           >
@@ -35,7 +35,7 @@
 
         <el-select v-model="type" placeholder="马驹类型">
           <el-option
-            v-for="item in typeOptions"
+            v-for="item in defaultHorseOptions"
             :key="item"
             :label="item"
             :value="item"
@@ -49,14 +49,14 @@
         <span class="currentTime">{{ currentTime }}</span>
         <span class="symbol">+</span>
         <el-input-number
-          v-model="time"
+          v-model="intervalTime"
           :min="0"
           placeholder="等待时间"
         ></el-input-number>
         <span class="min">分钟</span>
         <span class="symbol">=</span>
         <template>
-          <span v-if="time" class="endTime">{{ end }}</span>
+          <span v-if="intervalTime" class="endTime">{{ endTime }}</span>
           <span v-else class="currentTime">{{ currentTime }}</span>
         </template>
       </div>
@@ -66,106 +66,36 @@
       </div>
     </div>
     <div class="history">
-      <el-table :data="tableData" stripe style="width: 100%; height: 100%">
-        <el-table-column prop="originTime" label="记录时间" align="center">
-        </el-table-column>
-        <el-table-column prop="server" label="服务器" align="center">
-        </el-table-column>
-        <el-table-column prop="map" label="地点" align="center">
-        </el-table-column>
-        <el-table-column prop="type" label="类型" align="center">
-          <template slot-scope="scope">
-            <span v-html="showKeyWorld(scope.row.type)"></span>
-          </template>
-        </el-table-column>
-        <el-table-column prop="endTime" label="刷马时间" align="center">
-        </el-table-column>
-        <el-table-column label="操作" align="center">
-          <template slot-scope="scope">
-            <el-popconfirm
-              confirm-button-text="好的"
-              cancel-button-text="不用了"
-              icon="el-icon-info"
-              icon-color="red"
-              title="确定删除该条刷马信息？"
-              @confirm="onDeletRecord(scope.row)"
-            >
-              <el-button slot="reference" type="danger">删除</el-button>
-            </el-popconfirm>
-          </template>
-        </el-table-column>
-      </el-table>
+      <TableData :tableData="tableData" />
     </div>
   </div>
 </template>
 
 <script>
+import horseMapOptions from '@/assets/data/horseMap.json';
+import TableData from './tableData.vue';
 export default {
   name: 'Time',
+  components: {
+    TableData,
+  },
   data() {
     return {
       tableData: [],
       currentTime: '', // 当前时间
-      time: '',
-      end: '',
+      intervalTime: '', // 间隔时间
+      endTime: '', // 结束时间
       server: '', // 服务器
       map: '', // 地点
-      type: '', // 类型
+      type: '', // 马类型
 
-      serverList: ['破阵子'],
+      serverList: ['破阵子'], // 服务器列表
 
-      mapOptinos: [
-        {
-          label: '马场',
-          options: [
-            {
-              name: '黑戈壁',
-            },
-            {
-              name: '鲲鹏岛',
-            },
-            {
-              name: '阴山大草原',
-            },
-          ],
-        },
-        {
-          label: '大马',
-          options: [
-            {
-              name: '昆仑',
-            },
-            {
-              name: '南屏山',
-            },
-            {
-              name: '白龙口',
-            },
-            {
-              name: '无量山',
-            },
-            {
-              name: '黑龙沼',
-            },
-          ],
-        },
-        {
-          label: '小马',
-          options: [
-            {
-              name: '金水镇',
-            },
-            {
-              name: '巴陵县',
-            },
-            {
-              name: '龙门荒漠',
-            },
-          ],
-        },
-      ],
-      typeOptions: ['龙子/麟驹', '绝尘/赤蛇/闪电', '里飞沙', '赤兔'],
-      variationOptions: {
+      horseMapOptions, // 刷马地图数据
+      defaultHorseOptions: ['龙子/麟驹', '绝尘/赤蛇/闪电', '里飞沙', '赤兔'], // 默认马选项
+
+      // 变异马选项
+      variationHorseOptions: {
         鲲鹏岛: ['龙子/麟驹(变异)', '绝尘/赤蛇(变异)/闪电', '里飞沙', '赤兔'],
         阴山大草原: ['龙子/麟驹', '绝尘(变异)/赤蛇/闪电', '里飞沙', '赤兔'],
         黑戈壁: ['龙子(变异)/麟驹', '绝尘/赤蛇/闪电(变异)', '里飞沙', '赤兔'],
@@ -174,14 +104,18 @@ export default {
   },
 
   watch: {
-    time: {
+    intervalTime: {
       handler(val) {
         let currentTs = Math.round(new Date().getTime());
-        let ts = this.time * 60 * 1000;
+        let ts = this.intervalTime * 60 * 1000; // 间隔时间 换成时间戳
         let endTs = currentTs + ts;
-        this.end = this.getCurrentTime(endTs);
+        this.endTime = this.formatTime(endTs);
       },
       immediate: true,
+    },
+
+    currentTime: {
+      handler(val) {},
     },
 
     tableData: {
@@ -195,38 +129,67 @@ export default {
     map: {
       handler(val) {
         if (
-          this.mapOptinos
+          this.horseMapOptions
             .find((x) => x.label == '马场')
             .options.some((x) => x.name == val)
         ) {
-          this.typeOptions = this.variationOptions[val];
+          this.defaultHorseOptions = this.variationHorseOptions[val];
           this.type = '';
         } else if (
-          this.mapOptinos
+          this.horseMapOptions
             .find((x) => x.label == '小马')
             .options.some((x) => x.name == val)
         ) {
-          this.typeOptions = ['龙子/麟驹', '绝尘/赤蛇/闪电', '里飞沙', '赤兔'];
+          this.defaultHorseOptions = [
+            '龙子/麟驹',
+            '绝尘/赤蛇/闪电',
+            '里飞沙',
+            '赤兔',
+          ];
           this.type = '龙子/麟驹';
         } else if (val == '黑龙沼') {
-          this.typeOptions = ['龙子/麟驹', '绝尘/赤蛇/闪电', '里飞沙', '赤兔'];
+          this.defaultHorseOptions = [
+            '龙子/麟驹',
+            '绝尘/赤蛇/闪电',
+            '里飞沙',
+            '赤兔',
+          ];
           this.type = '里飞沙';
         } else if (
-          this.mapOptinos
+          this.horseMapOptions
             .find((x) => x.label == '大马')
             .options.some((x) => x.name == val)
         ) {
-          this.typeOptions = ['龙子/麟驹', '绝尘/赤蛇/闪电', '里飞沙', '赤兔'];
+          this.defaultHorseOptions = [
+            '龙子/麟驹',
+            '绝尘/赤蛇/闪电',
+            '里飞沙',
+            '赤兔',
+          ];
           this.type = '绝尘/赤蛇/闪电';
         }
       },
     },
   },
 
+  created() {
+    if (window.Notification) {
+      // 浏览器通知--window.Notification
+      if (Notification.permission == 'granted') {
+        console.log('允许通知');
+      } else if (Notification.permission != 'denied') {
+        console.log('需要通知权限');
+        Notification.requestPermission((permission) => {});
+      }
+    } else {
+      console.error('浏览器不支持Notification');
+    }
+  },
+
   mounted() {
     clearInterval(this.timer);
     this.timer = setInterval(() => {
-      this.currentTime = this.getCurrentTime(new Date().getTime());
+      this.currentTime = this.formatTime(new Date().getTime());
     }, 1000);
 
     if (window.localStorage.getItem('timeData')) {
@@ -240,11 +203,11 @@ export default {
     // 记录信息
     onClickRecord() {
       this.tableData.push({
-        originTime: this.currentTime,
+        recordTime: this.currentTime,
         server: this.server, // 服务器
         map: this.map, // 地点
         type: this.type, // 马的种类
-        endTime: this.end,
+        endTime: this.endTime,
       });
     },
 
@@ -260,12 +223,7 @@ export default {
       }
     },
 
-    // 删除
-    onDeletRecord(row) {
-      let index = this.tableData.findIndex((x) => x == row);
-      this.tableData.splice(index, 1);
-    },
-    getCurrentTime(time) {
+    formatTime(time) {
       let date = new Date(time);
       let year = date.getFullYear(); // 年
       let month = date.getMonth() + 1; // 月
