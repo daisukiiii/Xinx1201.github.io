669a1f4ae150baf4ca1224d02cca72f581861d52
Xinx1201 <z975203723@gmai.com>
2022-12-30 10:41:02 +0800

    fix: :memo: Jx3_Tool

--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ shell.exec("git push origin --delete gh-pages", {}, function () {
     shell.exec("npm run build", {}, function () {
       shell.exec("git checkout -b gh-pages", {}, function () {
         shell.exec("git add -f dist", {}, function () {
-          shell.exec("git commit -m 'tool'", {}, function () {
+          shell.exec("git commit -m 'Jx3_Tool'", {}, function () {
             shell.exec("git subtree push --prefix dist origin gh-pages", {}, function () {
               shell.exec("git checkout main", {}, function () {
               })
