a1e27aa748c85f4b76542dc74a9a081986fb68b0
Xinx1201 <z975203723@gmai.com>
2022-12-30 10:35:38 +0800

    feat: :sparkles: shelljs

---
+++ deploy.js
@@ -0,0 +1,17 @@
+const shell = require("shelljs")
+shell.exec("git push origin --delete gh-pages", {}, function () {
+  shell.exec("git branch -D gh-pages", {}, function () {
+    shell.exec("npm run build", {}, function () {
+      shell.exec("git checkout -b gh-pages", {}, function () {
+        shell.exec("git add -f dist", {}, function () {
+          shell.exec("git commit -m 'tool'", {}, function () {
+            shell.exec("git subtree push --prefix dist origin gh-pages", {}, function () {
+              shell.exec("git checkout main", {}, function () {
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+})
