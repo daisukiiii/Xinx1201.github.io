ebad012fec5f8bb233c1387eae3c149ca2dbb296
Xinx1201 <z975203723@gmai.com>
2023-01-04 18:50:07 +0800

    feat: :rocket: 添加阵营奇遇&统计

--- src/views/Adventure/Box.vue
+++ src/views/Adventure/Box.vue
@@ -7,23 +7,45 @@
       :key="index"
       @click.native="onClickIsFinish(item)"
     >
-      <!-- 背景 -->
-      <img :src="item.bg" class="bg" />
-      <!-- 中间 -->
-      <img :src="item.img" class="cover" />
-      <!-- 文字 -->
-      <img :src="item.text" class="text" />
-      <div
-        v-if="item.isFinish"
-        :style="{
-          backgroundColor: school
-            ? schools.find((x) => x.school == school).color
-            : '#8D90D8',
-        }"
-        class="finish"
-      >
-        已完成
-      </div>
+      <template v-if="item.type != 99">
+        <!-- 背景 -->
+        <img :src="item.bg" class="bg" />
+        <!-- 中间 -->
+        <img :src="item.img" class="cover" />
+        <!-- 文字 -->
+        <img :src="item.text" class="text" />
+        <div
+          v-if="item.isFinish"
+          :style="{
+            backgroundColor: school
+              ? schools.find((x) => x.school == school).color
+              : '#8D90D8',
+          }"
+          class="finish"
+        >
+          已完成
+        </div>
+      </template>
+
+      <template v-else>
+        <div class="container">
+          <div class="normal">
+            普通奇遇
+            {{
+              adventure.filter((x) => x.type == 1 && x.isFinish == true).length
+            }}/{{ adventure.filter((x) => x.type == 1).length }}
+          </div>
+          <div class="perfect">
+            绝世奇遇
+            {{
+              adventure.filter((x) => x.type == 0 && x.isFinish == true).length
+            }}/{{ adventure.filter((x) => x.type == 0).length }}
+          </div>
+          <div class="time">
+            {{ new Date().toLocaleDateString() }}
+          </div>
+        </div>
+      </template>
     </el-card>
   </div>
 </template>
@@ -38,7 +60,7 @@ export default {
     return {
       adventure,
       schools,
-      school: '', // 当前选中的门派
+      school: '霸刀', // 当前选中的门派
     };
   },
   mounted() {
@@ -49,6 +71,7 @@ export default {
         isFinish: false,
       };
     });
+    console.log(this.adventure);
   },
   methods: {
     check(type) {
@@ -56,54 +79,81 @@ export default {
         case 'all':
           // 勾选全部
           if (this.adventure.every((x) => x.isFinish == true)) {
-            this.adventure.forEach((x) => {
-              x.isFinish = false;
-            });
+            this.adventure
+              .filter((x) => x.type !== 99)
+              .forEach((x) => {
+                x.isFinish = false;
+              });
           } else {
-            this.adventure.forEach((x) => {
-              x.isFinish = true;
-            });
+            this.adventure
+              .filter((x) => x.type !== 99)
+              .forEach((x) => {
+                x.isFinish = true;
+              });
           }
           break;
         case 'normal':
           // 如果全部选中 置为未选中状态
           if (this.adventure.every((x) => x.isFinish == true)) {
-            this.adventure.forEach((x) => {
-              x.isFinish = false;
-            });
+            this.adventure
+              .filter((x) => x.type !== 99)
+              .forEach((x) => {
+                x.isFinish = false;
+              });
           }
           // 勾选普通奇遇
-          this.adventure.forEach((x) => {
-            if (x.type == 1) {
-              x.isFinish = !x.isFinish;
-            } else {
-              x.isFinish = false;
-            }
-          });
+          this.adventure
+            .filter((x) => x.type !== 99)
+            .forEach((x) => {
+              if (x.type == 1) {
+                x.isFinish = !x.isFinish;
+              } else {
+                x.isFinish = false;
+              }
+            });
           break;
         case 'perfect':
           // 如果全部选中 置为未选中状态
           if (this.adventure.every((x) => x.isFinish == true)) {
-            this.adventure.forEach((x) => {
-              x.isFinish = false;
-            });
+            this.adventure
+              .filter((x) => x.type !== 99)
+              .forEach((x) => {
+                x.isFinish = false;
+              });
           }
           // 勾选绝世奇遇
-          this.adventure.forEach((x) => {
-            if (x.type == 0) {
-              x.isFinish = !x.isFinish;
-            } else {
-              x.isFinish = false;
-            }
-          });
+          this.adventure
+            .filter((x) => x.type !== 99)
+            .forEach((x) => {
+              if (x.type == 0) {
+                x.isFinish = !x.isFinish;
+              } else {
+                x.isFinish = false;
+              }
+            });
           break;
         default:
           break;
       }
     },
     onClickIsFinish(item) {
-      this.adventure.find((x) => x.name == item.name).isFinish = !item.isFinish;
+      this.adventure
+        .filter((x) => x.type !== 99)
+        .find((x) => x.name == item.name).isFinish = !item.isFinish;
     },
+
+    // 阵营图片
+    changeCampColor(val) {
+      let str = `camp_${val.id}_open`;
+
+      // 黑白路替换掉对应的图片
+      this.adventure.forEach((x) => {
+        if (x.camp) {
+          x.img = x.img.replace(x.img.match(/camp_(\S*)_open/)[0], str);
+        }
+      });
+    },
+
     changeColor(val) {
       this.school = val.school;
 
@@ -130,6 +180,29 @@ export default {
     .el-card__body,
     .el-main {
       padding: 0px;
+    }
+  }
+
+  .container {
+    height: 140px;
+    line-height: 30px;
+    text-align: center;
+    .logo {
+      height: 50%;
+    }
+
+    .normal,
+    .perfect,
+    .time {
+      line-height: 45px;
+    }
+
+    .normal {
+      color: #b51cad;
+    }
+
+    .perfect {
+      color: #f7b500;
     }
   }
 
--- src/views/Adventure/index.vue
+++ src/views/Adventure/index.vue
@@ -4,8 +4,10 @@
       <div>
         <el-select
           filterable
+          class="school"
+          ref="school"
           v-model="school"
-          placeholder="请选择"
+          placeholder="请选择门派"
           value-key="school"
         >
           <el-option
@@ -15,6 +17,23 @@
             :value="item"
           >
             <span :style="{ color: item.color }">{{ item.school }}</span>
+          </el-option>
+        </el-select>
+        <el-select
+          filterable
+          class="camp"
+          ref="camp"
+          v-model="camp"
+          placeholder="请选择阵营"
+          value-key="id"
+        >
+          <el-option
+            v-for="item in pvps"
+            :key="item.id"
+            :label="item.camp"
+            :value="item"
+          >
+            <span :style="{ color: item.color }">{{ item.camp }}</span>
           </el-option>
         </el-select>
       </div>
@@ -31,6 +50,7 @@
 <script>
 import Box from './Box.vue';
 import schools from '@/assets/data/school.json';
+import pvps from '@/assets/data/pvp.json';
 export default {
   name: 'Adventure',
   components: {
@@ -39,6 +59,8 @@ export default {
   data() {
     return {
       schools,
+      pvps,
+      camp: '恶人谷',
       school: '霸刀',
     };
   },
@@ -46,12 +68,28 @@ export default {
     school: {
       handler(val) {
         if (val) {
+          // 改变选中后的颜色
+          this.$refs.school.$el.children[0].children[0].style.color = val.color;
           this.$refs.box.changeColor(val);
         }
       },
     },
+    camp: {
+      handler(val) {
+        if (val) {
+          // 改变选中后的颜色
+          this.$refs.camp.$el.children[0].children[0].style.color = val.color;
+          this.$refs.box.changeCampColor(val);
+        }
+      },
+    },
   },
-  mounted() {},
+  mounted() {
+    // 默认霸刀的颜色
+    this.$refs.school.$el.children[0].children[0].style.color = '#8D90D8';
+    // 默认恶人谷的颜色
+    this.$refs.camp.$el.children[0].children[0].style.color = 'red';
+  },
   methods: {
     onClickCheck(type) {
       this.$refs.box.check(type);
@@ -63,5 +101,8 @@ export default {
 <style lang="scss" scoped>
 .operation {
   margin-bottom: 10px;
+  .camp {
+    margin-left: 10px;
+  }
 }
 </style>
