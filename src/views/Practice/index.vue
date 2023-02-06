4e56b6e6186b67616339000d1686481286dc0fc7
Xinx1201 <z975203723@gmail.com>
2023-02-06 10:59:02 +0800

    chore: :art: 添加图标

--- src/views/Practice/index.vue
+++ src/views/Practice/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <el-tabs type="border-card" class="tabs-container">
-      <el-tab-pane v-for="item in tabs" :label="item.name" :key="item.key">
+      <el-tab-pane v-for="item in tabs" :key="item.key">
+        <span slot="label"><i :class="item.icon"></i> {{item.name}}</span>
         <Echarts v-if="item.key == 'Echarts'" />
         <TodoList v-if="item.key == 'TodoList'" />
         <Websocket v-if="item.key == 'Websocket'" />
@@ -31,6 +32,7 @@ export default {
     return {
       tabs: [
         {
+          icon:'el-icon-notebook-2',
           name: '代办列表',
           key: 'TodoList',
         },
@@ -39,6 +41,7 @@ export default {
           key: 'DebounceThrottle',
         },
         {
+          icon:'el-icon-s-data',
           name: 'Echarts图表',
           key: 'Echarts',
         },
@@ -47,6 +50,7 @@ export default {
           key: 'Websocket',
         },
         {
+          icon:'el-icon-eleme',
           name: 'ElementUI相关',
           key: 'ComponentsSkills',
         },
